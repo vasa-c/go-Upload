f8c9cfb56ba9fbfb8b643674004cab3d49636c4b
Grigoriev Oleg <go.vasac@gmail.com>
2013-04-26 18:53:29 +0400

    Images: Single and Multi types (blank)

---
+++ src/Images/Types/Multi.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Type: Multi
+ *
+ * @package go\Upload\Images
+ * @author  Grigoriev Oleg aka vasa_c <go.vasac@gmail.com>
+ */
+
+namespace go\Upload\Images\Types;
+
+class Multi extends Base
+{
+}
---
+++ src/Images/Types/Single.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Type: Single
+ *
+ * @package go\Upload\Images
+ * @author  Grigoriev Oleg aka vasa_c <go.vasac@gmail.com>
+ */
+
+namespace go\Upload\Images\Types;
+
+class Single extends Base
+{
+}
